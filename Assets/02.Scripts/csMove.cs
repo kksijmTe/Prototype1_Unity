7d8e91aadbbfbd4c7d782b6cec63df09f800700a
InJaeMyung <dlswo555@gmail.com>
2016-06-27 13:54:19 +0900

    커밋 실험

--- Assets/02.Scripts/csMove.cs
+++ Assets/02.Scripts/csMove.cs
@@ -5,11 +5,11 @@ public class csMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//커밋 실험 중 임
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//ㄱㅁㄴㅇㅀㄷㄹ
 	}
 }
